09c15e157b298d378dbea2fe5aa2a8c9ce6165eb
Michika <mhiratsuka0209@gmail.com>
2014-08-12 14:56:30 +0900

    Add isOpen and onSettingClick for menu

--- frontend/app/src/components/SideMenu/hook.ts
+++ frontend/app/src/components/SideMenu/hook.ts
@@ -1,8 +1,21 @@
 import { useMediaQuery } from '@mui/material'
 import { Theme } from '@mui/material/styles'
+import { useState } from 'react'
 
-export const useSideMenu = (): { isLargeScreen: boolean } => {
+export const useSideMenu = (): {
+  isLargeScreen: boolean
+  isOpen: boolean
+  onSettingClick: () => void
+} => {
+  const [open, setOpen] = useState(true)
+
+  const handleSettingClick = () => {
+    setOpen(!open)
+  }
+
   return {
     isLargeScreen: useMediaQuery((theme: Theme) => theme.breakpoints.up('lg')),
+    isOpen: open,
+    onSettingClick: handleSettingClick(),
   }
 }
