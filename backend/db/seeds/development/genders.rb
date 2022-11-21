9429972c72b28f130360626eb747aed9afe2dbfd
Michika <mhiratsuka0209@gmail.com>
2022-11-21 22:38:13 +0900

    Prevented adding deprecated data to genders table

--- backend/db/seeds/development/genders.rb
+++ backend/db/seeds/development/genders.rb
@@ -1,23 +1,25 @@
 # initial data for gender of pets
-Gender.create!(
-  [
-    {
-      name: "male",
-      code: 0,
-    },
-    {
-      name: "female",
-      code: 1,
-    },
-    {
-      name: "neutered male",
-      code: 2,
-    },
-    {
-      name: "spayed female",
-      code: 3,
-    }
-  ]
-)
-
-puts "genders = #{Gender.count}"
+if Gender.count == 0
+  Gender.create!(
+    [
+      {
+        name: "male",
+        code: 0,
+      },
+      {
+        name: "female",
+        code: 1,
+      },
+      {
+        name: "neutered male",
+        code: 2,
+      },
+      {
+        name: "spayed female",
+        code: 3,
+      }
+    ]
+  )
+else
+  puts "genders table is already available"
+end
