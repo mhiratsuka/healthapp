1b17094e9df552dc86234dd285b3e932096cfe7f
Michika <mhiratsuka0209@gmail.com>
2022-11-25 23:52:03 +0900

    Added the new route for pets

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope :api do
-    resources :users
+    resources :users do
+      resources :pets
+    end
   end
 end
