dc111078ff9fe360c92896dcbd19fd10ec41625f
Michika <mhiratsuka0209@gmail.com>
2022-12-13 22:45:38 +0900

    Modified imports

--- frontend/app/src/pages/Login/index.tsx
+++ frontend/app/src/pages/Login/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { Box } from '@mui/material'
-import { Layout } from './../../components/Layout'
+import { Layout } from '@/components/Layout'
 
 export const Login: FC = () => {
   return (
