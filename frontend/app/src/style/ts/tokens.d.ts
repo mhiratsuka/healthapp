4c9cba260fd6b6421f3147dacd86e037d092581a
Michika <mhiratsuka0209@gmail.com>
2022-12-25 23:09:06 +0900

    Added token-transformer package & created design tokens

---
+++ frontend/app/src/style/ts/tokens.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 25 Dec 2022 13:58:34 GMT
+ */
+
+export const Primary : string;
+export const Secondary : string;
+export const Warning : string;
+export const Info : string;
+export const Success : string;
