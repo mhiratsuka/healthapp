b1409b53fb22aee21c000aff0f9ad2148d3de5ba
Michika <mhiratsuka0209@gmail.com>
2023-01-01 23:56:33 +0900

    Added Table

--- frontend/app/src/pages/Journal/hooks.ts
+++ frontend/app/src/pages/Journal/hooks.ts
@@ -2,10 +2,12 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 
+
 export const UseJournal = () => {
   const [pets, setPets] = useState([])
 
   useEffect(() => {
+
     axios.get('http://localhost:8000/api/users/1/pets')
       .then(response => setPets(response.data))
       .catch(error => console.log(error))
