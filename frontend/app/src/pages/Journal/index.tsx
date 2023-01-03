690e1c7ab531ec10a9a963bae3896792a16d5b11
Michika <mhiratsuka0209@gmail.com>
2023-01-03 23:31:29 +0900

    Added a table in the Journal page

--- frontend/app/src/pages/Journal/index.tsx
+++ frontend/app/src/pages/Journal/index.tsx
@@ -1,4 +1,11 @@
 import { Box, Button, Typography } from '@mui/material'
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import { FC } from 'react'
 
 import { Layout } from '@/components/Layout'
@@ -9,12 +16,36 @@ import { UseJournal } from './hooks'
 export const Journal: FC = () => {
   const { pets } = UseJournal()
 
-  console.log(`pets ${pets}`)
 
   return (
   <Layout title={'journal'}>
     <Box display='flex' flexDirection='column' width='100%'>
       <Typography variant='h1' gutterBottom color={Primary}>Journal page</Typography>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Kind</TableCell>
+              <TableCell>Birthday</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {pets.map(({ name, kind, birthday }, idx) => (
+              <TableRow
+                key={idx}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {name}
+                </TableCell>
+                <TableCell align="right">{kind}</TableCell>
+                <TableCell align="right">{birthday}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
       <Button>Test</Button>
     </Box>
   </Layout>
