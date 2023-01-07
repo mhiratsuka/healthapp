977a3015fb5c494c76b42f4884908e1013e2b58c
Michika <mhiratsuka0209@gmail.com>
2023-01-07 23:57:15 +0900

    Added a navigation in the top page

--- frontend/app/src/pages/Top/index.tsx
+++ frontend/app/src/pages/Top/index.tsx
@@ -6,6 +6,12 @@ import { Layout } from '@/components/Layout'
 export const Top: FC = () => {
   return (
   <Layout title={'top'}>
+    <nav>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div>healthapp</div>
+        <div>login</div>
+      </Box>
+    </nav>
     <Box>Top page</Box>
   </Layout>
   )
