e0be98be53b0391285ce4b43a1dab6636ef10b6b
Michika <mhiratsuka0209@gmail.com>
2023-01-10 21:48:53 +0900

    Added fonts

--- frontend/app/src/style/ts/tokens.ts
+++ frontend/app/src/style/ts/tokens.ts
@@ -1,10 +1,13 @@
 /**
  * Do not edit directly
- * Generated on Sun, 25 Dec 2022 13:58:34 GMT
+ * Generated on Tue, 10 Jan 2023 12:35:49 GMT
  */
 
-export const Primary = '#006DCF'
-export const Secondary = '#9B61A3'
-export const Warning = '#FF8F48'
-export const Info = '#00A0FF'
-export const Success = '#0CA998'
+export const Primary = "#006DCF";
+export const Secondary = "#9B61A3";
+export const Warning = "#FF8F48";
+export const Info = "#00A0FF";
+export const LibreBaskerville = "Libre Baskerville";
+export const Success = "#0CA998";
+export const EbGaramond = "EB Garamond";
+export const Serif = "serif";
