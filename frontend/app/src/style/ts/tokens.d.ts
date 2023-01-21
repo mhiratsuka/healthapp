6b3594a14f62914ac4b110a614fc3ad1fb804eb0
Michika <mhiratsuka0209@gmail.com>
2023-01-21 22:09:44 +0900

    Fixed lint

--- frontend/app/src/style/ts/tokens.d.ts
+++ frontend/app/src/style/ts/tokens.d.ts
@@ -3,12 +3,12 @@
  * Generated on Sun, 15 Jan 2023 12:07:14 GMT
  */
 
-export const Primary : string;
-export const Secondary : string;
-export const Warning : string;
-export const Info : string;
-export const LibreBaskerville : string;
-export const Success : string;
-export const EbGaramond : string;
-export const Serif : string;
-export const Main : string;
+export const Primary: string
+export const Secondary: string
+export const Warning: string
+export const Info: string
+export const LibreBaskerville: string
+export const Success: string
+export const EbGaramond: string
+export const Serif: string
+export const Main: string
--- frontend/app/src/style/ts/tokens.ts
+++ frontend/app/src/style/ts/tokens.ts
@@ -3,12 +3,12 @@
  * Generated on Sun, 15 Jan 2023 12:07:14 GMT
  */
 
-export const Primary = "#006DCF";
-export const Secondary = "#9B61A3";
-export const Warning = "#FF8F48";
-export const Info = "#00A0FF";
-export const LibreBaskerville = "Libre Baskerville";
-export const Success = "#6ACE8C";
-export const EbGaramond = "EB Garamond";
-export const Serif = "serif";
-export const Main = "#0CA998";
+export const Primary = '#006DCF'
+export const Secondary = '#9B61A3'
+export const Warning = '#FF8F48'
+export const Info = '#00A0FF'
+export const LibreBaskerville = 'Libre Baskerville'
+export const Success = '#6ACE8C'
+export const EbGaramond = 'EB Garamond'
+export const Serif = 'serif'
+export const Main = '#0CA998'
