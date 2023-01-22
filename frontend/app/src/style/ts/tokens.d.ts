f98555571ef38151680ab0ccd1882f85a2a7db51
Michika <mhiratsuka0209@gmail.com>
2023-01-22 22:28:49 +0900

    Added typography to design tokens

--- frontend/app/src/style/ts/tokens.d.ts
+++ frontend/app/src/style/ts/tokens.d.ts
@@ -1,14 +1,28 @@
 /**
  * Do not edit directly
- * Generated on Sun, 15 Jan 2023 12:07:14 GMT
+ * Generated on Sun, 22 Jan 2023 13:22:25 GMT
  */
 
-export const Primary: string
-export const Secondary: string
-export const Warning: string
-export const Info: string
-export const LibreBaskerville: string
-export const Success: string
-export const EbGaramond: string
-export const Serif: string
-export const Main: string
+export const Primary : string;
+export const Secondary : string;
+export const Warning : string;
+export const Info : string;
+export const LibreBaskerville : string;
+export const Success : string;
+export const EbGaramond : string;
+export const Serif : string;
+export const Main : string;
+export const H1 : string;
+export const Headings1 : { fontFamily: string, fontSize: string };
+export const H2 : string;
+export const H3 : string;
+export const H4 : string;
+export const H5 : string;
+export const H6 : string;
+export const P : string;
+export const Heading2 : { fontFamily: string, fontSize: string };
+export const Heading3 : { fontFamily: string, fontSize: string };
+export const Heading4 : { fontFamily: string, fontSize: string };
+export const Heading5 : { fontFamily: string, fontSize: string };
+export const Heading6 : { fontFamily: string, fontSize: string };
+export const Ptag : { fontFamily: string, fontSize: string };
--- frontend/app/src/style/ts/tokens.ts
+++ frontend/app/src/style/ts/tokens.ts
@@ -1,14 +1,28 @@
 /**
  * Do not edit directly
- * Generated on Sun, 15 Jan 2023 12:07:14 GMT
+ * Generated on Sun, 22 Jan 2023 13:22:25 GMT
  */
 
-export const Primary = '#006DCF'
-export const Secondary = '#9B61A3'
-export const Warning = '#FF8F48'
-export const Info = '#00A0FF'
-export const LibreBaskerville = 'Libre Baskerville'
-export const Success = '#6ACE8C'
-export const EbGaramond = 'EB Garamond'
-export const Serif = 'serif'
-export const Main = '#0CA998'
+export const Primary = "#006DCF";
+export const Secondary = "#9B61A3";
+export const Warning = "#FF8F48";
+export const Info = "#00A0FF";
+export const LibreBaskerville = "Libre Baskerville";
+export const Success = "#6ACE8C";
+export const EbGaramond = "EB Garamond";
+export const Serif = "serif";
+export const Main = "#0CA998";
+export const H1 = "4rem";
+export const Headings1 = {"fontFamily":"Libre Baskerville","fontSize":"4rem"};
+export const H2 = "3.5rem";
+export const H3 = "3rem";
+export const H4 = "2.5rem";
+export const H5 = "2rem";
+export const H6 = "1.5rem";
+export const P = "1rem";
+export const Heading2 = {"fontFamily":"Libre Baskerville","fontSize":"3.5rem"};
+export const Heading3 = {"fontFamily":"Libre Baskerville","fontSize":"3rem"};
+export const Heading4 = {"fontFamily":"Libre Baskerville","fontSize":"2.5rem"};
+export const Heading5 = {"fontFamily":"Libre Baskerville","fontSize":"2rem"};
+export const Heading6 = {"fontFamily":"Libre Baskerville","fontSize":"1.5rem"};
+export const Ptag = {"fontFamily":"Libre Baskerville","fontSize":"1rem"};
