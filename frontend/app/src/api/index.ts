02f716b60a5eb62840dfbc4a6ce46140508895e2
Michika <mhiratsuka0209@gmail.com>
2023-02-03 23:38:49 +0900

    Modified Get API

--- frontend/app/src/api/index.ts
+++ frontend/app/src/api/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios'
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios'
 
 const client: AxiosInstance = axios.create({
   baseURL: 'http://localhost:3000',
@@ -7,9 +7,33 @@ const client: AxiosInstance = axios.create({
   timeout: 5000,
 })
 
-export const getData = async (URL: string): Promise<void> => {
+interface apiType {
+  path: string
+  error?: (err: AxiosError) => void
+}
+async function getRequest<T>(arg: getRequestApiType<T>): Promise<void> {
+  const headers = arg.accessToken
+    ? { Authorization: `Bearer ${arg.accessToken}` }
+    : {}
+  await axios
+    .get<T>(endpointUrl(arg.system, arg.path), {
+      headers,
+      params: arg.params || {},
+      paramsSerializer: (params) =>
+        qs.stringify(params, { arrayFormat: 'repeat' }),
+    })
+    .then(({ data }) => arg.callback?.(data))
+    .catch(arg.error)
+}
+
+export const getData = async (arg: apiType): Promise<void> => {
+  await client
+    .get(arg.path)
+    .then(({ data }) => arg.callback?.(data))
+    .catch(arg.error)
+
   try {
-    const response: AxiosResponse = await client.get(URL)
+    const response: AxiosResponse = await client.get(arg.path)
 
     console.log('show response.')
     console.log(response)
