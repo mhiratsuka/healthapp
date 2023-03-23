64b9781c7d75f0c57ef236c8fdf8a034b51842d0
Michika <mhiratsuka0209@gmail.com>
2023-03-23 09:34:21 +0900

    Set pet id to the select box

--- frontend/app/src/pages/Journal/PetSelect.tsx
+++ frontend/app/src/pages/Journal/PetSelect.tsx
@@ -1,18 +1,25 @@
 import MenuItem from '@mui/material/MenuItem'
 import Select from '@mui/material/Select'
-import React, { FC } from 'react'
+import { FC, useState, useEffect } from 'react'
 
 export const PetSelect: FC<{
   value: { id: number; name: string }
   onChange: (option: { id: number; name: string } | null) => void
   options: Array<{ id: number; name: string }>
 }> = ({ value, onChange, options }) => {
+  const [pet, setPet] = useState(value ?? { id: value.id, name: value.name })
+
+  useEffect(() => {
+    setPet({ id: value?.id, name: value?.name })
+  }, [value])
+
   return (
     <Select
       labelId='demo-simple-select-label'
       id='demo-simple-select'
       defaultValue={value.id}
       onChange={(_, value) => onChange(value)}
+      value={pet.id}
     >
       {options.map(({ id, name }, idx) => (
         <MenuItem value={id} key={idx}>
