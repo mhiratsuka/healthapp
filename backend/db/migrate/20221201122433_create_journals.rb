2af703c4c38f04275e25b5efb1a42d328ed01f0b
Michika <mhiratsuka0209@gmail.com>
2023-04-26 17:58:23 +0900

    Fixed Journals schema

--- backend/db/migrate/20221201122433_create_journals.rb
+++ backend/db/migrate/20221201122433_create_journals.rb
@@ -2,8 +2,8 @@ class CreateJournals < ActiveRecord::Migration[6.1]
   def change
     create_table :journals do |t|
       t.string :title
-      t.date :from_date
-      t.date :to_date
+      t.datetime :from_date
+      t.datetime :to_date
       t.text :note
       t.string :category
 
