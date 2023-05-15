3bb377bcffa1a51234cec410ffcbd83a717bf712
Michika <mhiratsuka0209@gmail.com>
2023-05-15 14:20:16 +0900

    Added return value for confirmDialog

--- frontend/app/src/pages/Journal/hooks.ts
+++ frontend/app/src/pages/Journal/hooks.ts
@@ -37,7 +37,7 @@ export const UseJournal = (): {
   const [recordModalOpen, setRecordModalOpen] = useState(false)
   const [pets, setPets] = useState([])
   const [selectPet, setSelectPet] = useState<{ id: number; name: string }>()
-  const [confirmDialog, setConfirmDialog] = useState<journalType>()
+  const [confirmDialogValue, setConfirmDialogValue] = useState<journalType>()
 
   const {
     register,
@@ -178,6 +178,8 @@ export const UseJournal = (): {
       onChange: setSelectPet,
       options: pets,
     },
-    confirmDialog: {},
+    confirmDialog: {
+      value: confirmDialogValue,
+    },
   }
 }
