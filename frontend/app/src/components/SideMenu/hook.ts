315bbcc9a8edab86ed7f96040de46935595b188c
Michika <mhiratsuka0209@gmail.com>
2023-08-14 15:11:46 +0900

    Changed the default dtate

--- frontend/app/src/components/SideMenu/hook.ts
+++ frontend/app/src/components/SideMenu/hook.ts
@@ -7,15 +7,15 @@ export const useSideMenu = (): {
   isOpen: boolean
   onSettingClick: () => void
 } => {
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(false)
 
-  const handleSettingClick = () => {
+  const handleSettingClick = (): void => {
     setOpen(!open)
   }
 
   return {
     isLargeScreen: useMediaQuery((theme: Theme) => theme.breakpoints.up('lg')),
     isOpen: open,
-    onSettingClick: handleSettingClick(),
+    onSettingClick: handleSettingClick,
   }
 }
