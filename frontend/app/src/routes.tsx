dbaaa97e63ec3de4edc9dcc29a4115b2e1c97785
Michika <mhiratsuka0209@gmail.com>
2023-09-10 19:29:35 +0900

    Fixed importing Top page

--- frontend/app/src/routes.tsx
+++ frontend/app/src/routes.tsx
@@ -5,7 +5,8 @@ import { Journal } from '@/pages/Journal'
 import { JournalDetail } from '@/pages/JournalDetail'
 import { Login } from '@/pages/Login'
 import { Pets } from '@/pages/Pets'
-import { Top } from '@/pages/Top'
+
+import { Top } from '@/pages/Top '
 
 export const routes = (
   <Routes>
