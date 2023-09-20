6cb61fa09dc9ed5aa329d47b55d95dcc5de80bcc
Michika <mhiratsuka0209@gmail.com>
2023-09-20 13:26:01 +0900

    Removed unused selected menu function

--- frontend/app/src/components/SideMenu/hook.ts
+++ frontend/app/src/components/SideMenu/hook.ts
@@ -1,6 +1,6 @@
 import { useMediaQuery } from '@mui/material'
 import { Theme } from '@mui/material/styles'
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 export const useSideMenu = (): {
   isLargeScreen: boolean
@@ -8,15 +8,9 @@ export const useSideMenu = (): {
   isMenuOpen: boolean
   isSettingOpen: boolean
   onSettingClick: () => void
-  selectedMenu: number
-  onMenuClick: (
-    event: React.MouseEvent<HTMLElement, MouseEvent>,
-    index: number
-  ) => void
 } => {
   const [menuOpen, setMenuOpen] = useState(false)
   const [open, setOpen] = useState(false)
-  const [selectedMenu, setSelectedMenu] = useState(0)
 
   const handleMenuToggle = (): void => {
     setMenuOpen(!menuOpen)
@@ -26,25 +20,11 @@ export const useSideMenu = (): {
     setOpen(!open)
   }
 
-  const handleMenuClick = (
-    event: React.MouseEvent<HTMLElement, MouseEvent>,
-    index: number
-  ): void => {
-    setSelectedMenu(index)
-  }
-
   return {
     isLargeScreen: useMediaQuery((theme: Theme) => theme.breakpoints.up('lg')),
     onMenuHandleToggle: handleMenuToggle,
     isMenuOpen: menuOpen,
     isSettingOpen: open,
     onSettingClick: handleSettingClick,
-    selectedMenu,
-    onMenuClick: (
-      event: React.MouseEvent<HTMLElement, MouseEvent>,
-      index: number
-    ) => {
-      handleMenuClick(event, index)
-    },
   }
 }
