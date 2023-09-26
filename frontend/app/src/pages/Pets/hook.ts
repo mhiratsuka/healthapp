528436d24206e4ba88654cdb2febffedc5c81a91
Michika <mhiratsuka0209@gmail.com>
2023-09-26 14:37:50 +0900

    Called pets data

--- frontend/app/src/pages/Pets/hook.ts
+++ frontend/app/src/pages/Pets/hook.ts
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 import { petType } from './model'
 
@@ -6,6 +7,19 @@ export const usePets = (): {
   pets: petType[]
 } => {
   const [pets, setPets] = useState([])
+
+  const getPetData = (): void => {
+    axios
+      .get(`http://localhost:8000/api/users/1/pets`)
+      .then((res) => {
+        setPets(res.data.data)
+      })
+      .catch((e) => console.log(e))
+  }
+
+  useEffect(() => {
+    getPetData()
+  }, [])
 
   return {
     pets,
