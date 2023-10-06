c9768e74ff34f00867dc9618a5c0277f5f4c3677
Michika <mhiratsuka0209@gmail.com>
2023-10-06 15:54:08 +0900

    Updated journal model for JournalDetail page

--- frontend/app/src/pages/JournalDetail/model.ts
+++ frontend/app/src/pages/JournalDetail/model.ts
@@ -1,9 +1,11 @@
 export interface journalType {
-  id?: number
+  id: number
   title: string
   from_date: Date
   to_date: Date
   category: string
   note: string
-  pet_id?: number
+  pet_id: number
+  created_at: Date
+  update_at: Date
 }
