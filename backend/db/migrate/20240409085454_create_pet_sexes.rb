e206a3232c93cb4794fb44720ae5f58f1731e362
Michika <mhiratsuka0209@gmail.com>
2023-10-21 18:24:18 +0900

    Updated pet sex migration file

--- backend/db/migrate/20240409085454_create_pet_sexes.rb
+++ backend/db/migrate/20240409085454_create_pet_sexes.rb
@@ -2,6 +2,9 @@ class CreatePetSexes < ActiveRecord::Migration[6.1]
   def change
     create_table :pet_sexes do |t|
 
+      t.references :sex, null: false, foreign_key: true
+      t.references :pet, null: false, foreign_key: true
+
       t.timestamps
     end
   end
