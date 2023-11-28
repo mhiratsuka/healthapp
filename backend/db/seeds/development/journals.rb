7ba5e67fb8fb2b421c3b0cd0571927a3a8980e36
Michika <mhiratsuka0209@gmail.com>
2023-11-28 17:31:26 +0900

    Added seeds

--- backend/db/seeds/development/journals.rb
+++ backend/db/seeds/development/journals.rb
@@ -25,6 +25,30 @@ if Journal.count == 0
         note: "test text 3",
         category: "test1",
         pet_id: 1
+      },
+      {
+        title: "Journal 4",
+        from_date: "2023-12-09 05:00:00+00",
+        to_date: "2023-12-10 03:00:00+00",
+        note: "test text 4",
+        category: "test4",
+        pet_id: 1
+      },
+      {
+        title: "Journal 5",
+        from_date: "2023-01-02 05:00:00+00",
+        to_date: "2023-01-03 03:00:00+00",
+        note: "test text 5",
+        category: "test5",
+        pet_id: 3
+      },
+      {
+        title: "Journal 6",
+        from_date: "2023-12-20 05:00:00+00",
+        to_date: "2023-12-20 03:00:00+00",
+        note: "test text 6",
+        category: "test6",
+        pet_id: 1
       }
     ]
   )
