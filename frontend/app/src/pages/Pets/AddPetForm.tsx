1aea39ea3f8b7640225a91014593abdaab4c32ec
Michika <mhiratsuka0209@gmail.com>
2023-12-11 09:54:51 +0900

    Temporally deleted modal button

--- frontend/app/src/pages/Pets/AddPetForm.tsx
+++ frontend/app/src/pages/Pets/AddPetForm.tsx
@@ -1,4 +1,3 @@
-import Button from '@mui/material/Button'
 import Dialog from '@mui/material/Dialog'
 import DialogTitle from '@mui/material/DialogTitle'
 import FormControl from '@mui/material/FormControl'
@@ -12,22 +11,6 @@ export const AddPetForm: FC<{
     <FormControl>
       <Dialog open={isOpen} onClose={onClose}>
         <DialogTitle>New Pet</DialogTitle>
-        <Button
-          onClick={onClose}
-          variant='outlined'
-          color='success'
-          disabled={disableCancelButton}
-        >
-          Cancel
-        </Button>
-        <Button
-          onClick={onSubmit}
-          variant='contained'
-          color='success'
-          disabled={disableSubmitButton}
-        >
-          Save
-        </Button>
       </Dialog>
     </FormControl>
   )
