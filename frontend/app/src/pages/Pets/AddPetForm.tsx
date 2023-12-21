8e7f32913a6df01cdc693bb2387993a9c7869e0f
Michika <mhiratsuka0209@gmail.com>
2023-12-21 11:18:45 +0900

    Added name textfield for pet modal

--- frontend/app/src/pages/Pets/AddPetForm.tsx
+++ frontend/app/src/pages/Pets/AddPetForm.tsx
@@ -1,17 +1,38 @@
-import { DialogActions, Button, DialogContentText } from '@mui/material'
+import {
+  DialogActions,
+  Button,
+  DialogContentText,
+  TextField,
+} from '@mui/material'
 import Dialog from '@mui/material/Dialog'
 import DialogTitle from '@mui/material/DialogTitle'
 import { FC } from 'react'
+import { UseFormRegister } from 'react-hook-form'
+
+import { REQUIRED_MESSAGE } from '@/domain/error'
+
+import { petType } from '../Journal/model'
 
 export const AddPetForm: FC<{
   isOpen: boolean
   onOpen: () => void
   onClose: () => void
-}> = ({ isOpen, onClose }) => {
+  register: UseFormRegister<petType>
+}> = ({ isOpen, onOpen, onClose, register }) => {
   return (
     <Dialog open={isOpen} onClose={onClose}>
       <DialogTitle>New Pet</DialogTitle>
       <DialogContentText>Please enter your new pet.</DialogContentText>
+      <TextField
+        autoFocus
+        margin='dense'
+        id='name'
+        label='Name'
+        type='text'
+        fullWidth
+        required
+        {...register('name', { required: REQUIRED_MESSAGE })}
+      />
       <DialogActions>
         <Button onClick={onClose}>Cancel</Button>
         <Button onClick={onClose}>Save</Button>
